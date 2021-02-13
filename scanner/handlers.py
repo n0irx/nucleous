b29eda8007792541b54b8b598f843a600759a5b1
nataprawiraf <nataprawiraf@gmail.com>
2021-02-13 12:05:06 +0700

    graph can be shown for template_used

--- scanner/handlers.py
+++ scanner/handlers.py
@@ -1,5 +1,10 @@
+import io
 import subprocess
 import datetime
+import urllib
+import base64
+
+NO_RESULT_STRING = "No results found. Happy hacking!"
 
 
 def run_nuclei_scan(domain, list_template):
@@ -21,4 +26,90 @@ def run_nuclei_scan(domain, list_template):
 
     result = nuclei_stderr + nuclei_stdout
 
-    return result
+    # formatting output
+    nuclei_output_splitted = result.split("\n")
+
+    nuclei_outputs = nuclei_output_splitted[12:]
+    nuclei_result = ""
+    nuclei_infos = []
+    nuclei_findings = []
+    nuclei_template_stat = ""
+
+    # seperate [INF] section with finding section
+    for nuclei_output in nuclei_outputs:
+        nuclei_output_splitted = nuclei_output.split(" ")
+        if nuclei_output_splitted[0] == "[INF]":
+            nuclei_infos.append(nuclei_output)
+        else:
+            if nuclei_output:
+                nuclei_findings.append(nuclei_output)
+
+    # if no result found after running nuclei scan
+    if NO_RESULT_STRING.lower() in nuclei_infos[-1].lower():
+        nuclei_result = NO_RESULT_STRING
+        nuclei_template_stat = nuclei_infos[-2]
+        nuclei_infos = nuclei_infos[:-2]
+
+    # if result found after running nuclei scan
+    else:
+        nuclei_result = "Result Found"
+        nuclei_template_stat = nuclei_infos[-1]
+        nuclei_infos = nuclei_infos[:-1]
+
+    # create list of dict contain template used information
+    nuclei_infos_as_dict = []
+    for nuclei_info in nuclei_infos:
+        nuclei_info_splitted = nuclei_info.split(" ")
+        nuclei_infos_as_dict.append(
+            {
+                "id": nuclei_info_splitted[1],
+                "desc": " ".join(nuclei_info_splitted[2:-2]),
+                "severity": nuclei_info_splitted[-1],
+            }
+        )
+
+    # create list of dict contain finding information
+    nuclei_findings_as_dict = []
+    for nuclei_finding in nuclei_findings:
+        nuclei_finding_splitted = nuclei_finding.split(" ")
+        template_subtemplate = nuclei_finding_splitted[0]
+        finding_splitted = template_subtemplate.split(":")
+        nuclei_findings_as_dict.append(
+            {
+                "template": finding_splitted[0].replace("[", ""),
+                "sub_template": finding_splitted[1].replace("]", ""),
+                "protocol": nuclei_finding_splitted[1],
+                "severity": nuclei_finding_splitted[2],
+                "path": nuclei_finding_splitted[2],
+            }
+        )
+
+    # get pie chart for each section
+
+    # pie chart for template used
+    severity_template_counter = {}
+    for nuclei_info_as_dict in nuclei_infos_as_dict:
+        if nuclei_info_as_dict["severity"] not in severity_template_counter:
+            severity_template_counter[nuclei_info_as_dict["severity"]] = 0
+        severity_template_counter[nuclei_info_as_dict["severity"]] += 1
+
+    total_template = len(nuclei_infos_as_dict)
+
+    template_labels = []
+    template_y = []
+
+    for severity, counter in severity_template_counter.items():
+        template_labels.append(severity)
+        template_y.append(counter)
+
+    template_used_data = [template_labels, template_y]
+
+    print(template_used_data)
+
+    return (
+        nuclei_template_stat,
+        nuclei_result,
+        nuclei_infos_as_dict,
+        nuclei_findings_as_dict,
+        template_used_data,
+    )
--- scanner/views.py
+++ scanner/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render, redirect
 
 from .handlers import run_nuclei_scan
 
-NO_RESULT_STRING = "No results found. Happy hacking!"
-
 
 def nuclei_index(request):
     files = glob.glob("nuclei-templates" + "/**/*.yaml", recursive=True)
@@ -32,61 +30,13 @@ def nuclei_scan(request):
         list_template = request.POST.getlist("list_template")
 
         # run nuclei scan via handler
-
-        nuclei_scan_result = run_nuclei_scan(domain, list_template)
-        nuclei_output_splitted = nuclei_scan_result.split("\n")
-
-        nuclei_outputs = nuclei_output_splitted[12:]
-        nuclei_result = ""
-        nuclei_infos = []
-        nuclei_findings = []
-        nuclei_template_stat = ""
-
-        for nuclei_output in nuclei_outputs:
-            nuclei_output_splitted = nuclei_output.split(" ")
-            if nuclei_output_splitted[0] == "[INF]":
-                nuclei_infos.append(nuclei_output)
-            else:
-                if nuclei_output:
-                    nuclei_findings.append(nuclei_output)
-
-        # if no result found
-        if NO_RESULT_STRING.lower() in nuclei_infos[-1].lower():
-            nuclei_result = NO_RESULT_STRING
-            nuclei_template_stat = nuclei_infos[-2]
-        # if result found
-        else:
-            nuclei_result = "Result Found"
-            nuclei_template_stat = nuclei_infos[-1]
-
-        nuclei_infos_as_dict = []
-
-        # info about scanned template
-        for nuclei_info in nuclei_infos:
-            nuclei_info_splitted = nuclei_info.split(" ")
-            nuclei_infos_as_dict.append(
-                {
-                    "id": nuclei_info_splitted[1],
-                    "desc": " ".join(nuclei_info_splitted[2:-2]),
-                    "severity": nuclei_info_splitted[-1],
-                }
-            )
-
-        nuclei_findings_as_dict = []
-        # info about finding for mat [tempalte_name:vuln_name]
-        for nuclei_finding in nuclei_findings:
-            nuclei_finding_splitted = nuclei_finding.split(" ")
-            template_subtemplate = nuclei_finding_splitted[0]
-            finding_splitted = template_subtemplate.split(":")
-            nuclei_findings_as_dict.append(
-                {
-                    "template": finding_splitted[0].replace("[", ""),
-                    "sub_template": finding_splitted[1].replace("]", ""),
-                    "protocol": nuclei_finding_splitted[1],
-                    "severity": nuclei_finding_splitted[2],
-                    "path": nuclei_finding_splitted[2],
-                }
-            )
+        (
+            nuclei_template_stat,
+            nuclei_result,
+            nuclei_infos_as_dict,
+            nuclei_findings_as_dict,
+            template_used_data,
+        ) = run_nuclei_scan(domain, list_template)
 
         return render(
             request,
@@ -96,6 +46,7 @@ def nuclei_scan(request):
                 "nuclei_result": nuclei_result,
                 "nuclei_infos_as_dict": nuclei_infos_as_dict,
                 "nuclei_findings_as_dict": nuclei_findings_as_dict,
+                "template_used_data": template_used_data,
             },
         )
     else:
